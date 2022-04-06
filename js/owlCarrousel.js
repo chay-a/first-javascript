cea9bd7d0e561b9684330299f1ad8a257c1f5e70
Chay Auriane <chaya.07@protonmail.com>
2022-04-06 10:14:19 +0200

    feat : carrousel img #3

--- js/owlCarrousel.js
+++ js/owlCarrousel.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
-        animateOut: 'fadeOut'
+        autoplay: true,
+        items: 3,
+        autoplayTimeout: 3000,
+        autoplayHoverPause:true,
+        loop: true,
+        margin: 30
     });
 })
