079f8cc54c91ab35d0c4afcbf4d4c8e4852844fe
Chay Auriane <chaya.07@protonmail.com>
2022-04-07 09:23:30 +0200

    feat : slider animation stop on hover and previous button #8

--- js/carrousel.js
+++ js/carrousel.js
@@ -2,33 +2,60 @@ let slideShow = 0;
 let isSliderRunning = true;
 
 document.addEventListener("DOMContentLoaded", function (event) {
+    const slidershow = document.querySelector('.slidershow')
     const buttonPrevious = document.querySelector('.previous');
     const buttonNext = document.querySelector('.next');
     slider();
-    buttonNext.addEventListener('click', function(){
-        isSliderRunning = false;
-        slideShow +=1;
-        slider();
+
+    slidershow.addEventListener('mouseover', function () {
+        slideAnimation(false);
     });
+
+    slidershow.addEventListener('mouseout', function () {
+        slideAnimation(true);
+    });
+
+    slidershow.addEventListener('mouseleave', function () {
+        slider();
+    })
+
+    buttonNext.addEventListener('click', function () {
+        navigation(1);
+    });
+
+    buttonPrevious.addEventListener('click', function () {
+        navigation(-1);
+    })
 })
 
 function slider() {
     const slides = document.querySelectorAll('.image');
     if (slideShow >= slides.length) {
         slideShow = 0;
-    } else if (slideShow < 0) {
-        slideShow = slides.length;
+    }
+    if (slideShow < 0) {
+        slideShow = slides.length - 1;
     }
     for (const slide of slides) {
         slide.style.display = 'none';
     }
+    console.log(slideShow);
     slides[slideShow].style.display = 'block';
-    slideShow++;
-    if(isSliderRunning) {
-        setTimeout(function() {
+    if (isSliderRunning) {
+        slideShow++;
+        setTimeout(function () {
             slider()
         }, 2000)
-    } else {
-        isSliderRunning = true;
     }
+}
+
+function navigation(slideShowValue) {
+    slideShow += slideShowValue;
+    slider();
+}
+
+function slideAnimation(valueSliderRunning) {
+    isSliderRunning = valueSliderRunning;
+    const slider = document.querySelector('.slider');
+    slider.classList.toggle('animation');
 }
