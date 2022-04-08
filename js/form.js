0132dc903b2bd2f8f188ce18890fa0dc34709264
Chay Auriane <chaya.07@protonmail.com>
2022-04-08 11:32:49 +0200

    feat : display errors

--- js/form.js
+++ js/form.js
@@ -1,10 +1,12 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     const submit = document.getElementById('submit');
-    submit.addEventListener('click', function(e){
+    submit.addEventListener('click', function (e) {
         e.preventDefault();
-        
-        const errors = validateForm();
-        
+
+        let errors = validateForm();
+        if (Object.entries(errors).length !== 0) {
+            displayErrors(errors);
+        }
     })
 });
 
@@ -20,7 +22,7 @@ function validateForm() {
     errors.date = dateValidate(newBook.date);
     errors.description = textValidate(newBook.description, 50, 300);
     errors.cover = imageValidate(newBook.cover);
-    
+
     errors = checkErrors(errors);
     return errors;
 }
@@ -69,29 +71,47 @@ function checkErrors(errors) {
     return errors;
 }
 
-const errorMsg = {
-    title: {
-        empty: 'Le titre ne peut pas être vide',
-        min: 'Le titre doit au moins contenir 5 caractères',
-        max: 'Le titre ne peut pas faire plus de 50 caractères',
-        special: 'Le titre ne peut pas contenir que des caratères spéciaux',
-        number: 'Le titre ne peut pas contenir que des chiffres',
-    },
-    date: {
-        empty: 'La date ne peut pas être vide',
-        format: 'La date n\'est pas au bon format',
-        today: 'La date ne peut pas être après aujourdh\'ui',
-    },
-    description: {
-        empty: 'La description ne peut pas être vide',
-        min: 'La description doit au moins contenir 50 caractères',
-        max: 'La description ne peut pas faire plus de 300 caractères',
-        special: 'La description ne peut pas contenir que des caratères spéciaux',
-        number: 'La description ne peut pas contenir que des chiffres',
-    },
-    cover: {
-        empty: 'La couverture ne peut pas être vide',
-        format: 'La couverture n\'est pas au bon format (formats acceptés : jpg, jpeg, png)',
-        size: 'La couverture fait plus de 100ko',
-    },
-};
+function printErrors(errors) {
+    deleteOldErrors();
+    const errorMsg = {
+        title: {
+            empty: 'Le titre ne peut pas être vide',
+            min: 'Le titre doit au moins contenir 5 caractères',
+            max: 'Le titre ne peut pas faire plus de 50 caractères',
+            special: 'Le titre ne peut pas contenir que des caratères spéciaux',
+            number: 'Le titre ne peut pas contenir que des chiffres',
+        },
+        date: {
+            empty: 'La date ne peut pas être vide',
+            format: 'La date n\'est pas au bon format',
+            today: 'La date ne peut pas être après aujourdh\'ui',
+        },
+        description: {
+            empty: 'La description ne peut pas être vide',
+            min: 'La description doit au moins contenir 50 caractères',
+            max: 'La description ne peut pas faire plus de 300 caractères',
+            special: 'La description ne peut pas contenir que des caratères spéciaux',
+            number: 'La description ne peut pas contenir que des chiffres',
+        },
+        cover: {
+            empty: 'La couverture ne peut pas être vide',
+            format: 'La couverture n\'est pas au bon format (formats acceptés : jpg, jpeg, png)',
+            size: 'La couverture fait plus de 100ko',
+        },
+    };
+    for (const key in errors) {
+        const input = document.getElementById(key);
+        const p = document.createElement('p');
+        p.textContent = errorMsg[key][errors[key]];
+        p.classList.add('error');
+        input.after(p);
+    }
+}
+
+function deleteOldErrors() {
+    const errors = document.querySelectorAll('.error');
+
+    errors.forEach(error => {
+        error.remove();
+    });
+}
