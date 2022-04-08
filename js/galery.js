ccf2c4c84ccaffcebf7343bea8b32a1ec81cb5b1
Chay Auriane <chaya.07@protonmail.com>
2022-04-08 15:46:06 +0200

    feat : add new image #6

--- js/galery.js
+++ js/galery.js
@@ -8,10 +8,43 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     columns.addEventListener('click', function () {
         toggleView('row')
-    })
+    });
+    let i = 0;
+    const send = document.getElementById('imgSend');
+    const add = document.getElementById('add');
+    add.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (i < 5) {
+            const form = document.getElementById('formImg');
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.classList.add('img-input');
+            form.append(input);
+            i++;
+        } else {
+            console.log('non');
+        }
+        send.style.display = 'block';
+    });
+    send.addEventListener('click', function (e) {
+        e.preventDefault();
+        const imgUrl = document.querySelectorAll('.img-input');
+        for (const img  of imgUrl) {
+            if (imgUrlValidate(img.value)) {
+                displayImg(img);
+            }
+        }
+    });
 })
 
 function toggleView(direction) {
     const instagram = document.querySelector('.instagram');
-        instagram.style.flexDirection = direction;
+    instagram.style.flexDirection = direction;
+}
+
+function displayImg(img) {
+    const instagram = document.querySelector('.instagram');
+    const image = document.createElement('img');
+    image.setAttribute('src', img.value);
+    instagram.prepend(image);
 }
