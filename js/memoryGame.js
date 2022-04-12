daf31781f2347adeeab32665ff4c37ee99ddf5dd
Chay Auriane <chaya.07@protonmail.com>
2022-04-12 10:12:32 +0200

    feat : check same cards #10

--- js/memoryGame.js
+++ js/memoryGame.js
@@ -1,8 +1,38 @@
+let cardFlipped = 0;
+
 function gameStart() {
     const cards = document.querySelectorAll('.card');
-    cards.forEach(card => card.addEventListener('click', flip))
+    cards.forEach(card => card.addEventListener('click', flip));
 }
 
 function flip() {
-    this.classList.toggle('flip');
+    cardFlipped++;
+    if (cardFlipped <= 2) {
+        this.classList.add('flip');
+    }
+    if (cardFlipped == 2) {
+        checkSameCards();
+        cardFlipped = 0;
+    }
+}
+
+
+function checkSameCards() {
+    const flippedCards = document.querySelectorAll('.flip');
+    console.log(flippedCards[0],flippedCards[1]);
+    if (flippedCards[0].firstChild.getAttribute('src') !== flippedCards[1].firstChild.getAttribute('src')) {
+        setTimeout(function () {
+            removeFlip(flippedCards);
+        }, 1000);
+    }
+}
+
+function removeFlip(flippedCards) {
+    flippedCards.forEach(flippedCard => {
+        flippedCard.classList.remove('flip');
+    });
+}
+
+{
+
 }
